d218eb6f6af54f7eb3db473f32fa37da8fc3fcc0
Joseph McCarron <joe@zendesk.com>
2015-02-27 16:06:51 -0800

    two way chat!

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
+/*global WebSocket*/
+/*global EventSource*/
 (function() {
-
   return {
     client_id: 'd3460d081d3f4ecaba722b26634cd546f11ea2af51e8cb00d9f8a3674551f5fc',
     client_secret: 'ec8e6ec87b41634fead10918573447e1090ce07ff0336caf22fd5e8e01924b70',
     fdRoot: 'https://api.flowdock.com',
+    // defaultState: 'home',
     events: {
       'app.created':            'start',
 
       'click .ping_home':       'start',
       'click .logout':          'logout',
 
+      'click .launch_flowdock': 'loadFlowdock',
+
       'click .login-flowdock':  'loginFlowdock',
-      // 'loginFlowdock.done':     'loggedinFlowdock',
       'loadFlowdock.done':      'loadedFlowdock',
-      'click button.flowdock':  'postFlowdock',
+      'click button.ping_flowdock':  'postFlowdock',
       // 'postFlowdock.done':      'postedFlowdock',
+      'click button.reply_flowdock':'replyFlowdock',
+      'keydown .reply_flowdock': function(e) {
+        if(e.which == 13) { // if Enter pressed
+          this.replyFlowdock(e);
+        }
+      },
+
+      'app.willDestroy': 'close',
 
       'loadHall.done':          'hallLoaded',
       'postToHall.done':        'hallPosted',
@@ -27,20 +38,18 @@
     },
     requests: {
       // ### Flowdock
-      loginFlowdock:    function(creds) {
-        console.log(creds);
-        var data = JSON.stringify({
-          "client_id": this.client_id,
-          "client_secret": this.client_secret,
-          "grant_type": "password",
-          "scope": "flow private",
-          "username": creds.username,
-          "password": creds.password
-        });
+      loginFlowdock: function(creds) {
         return {
           url: this.fdRoot + '/oauth/token',
           type: 'POST',
-          data: data,
+          data: JSON.stringify({
+            "client_id": this.client_id,
+            "client_secret": this.client_secret,
+            "grant_type": "password",
+            "scope": "flow private",
+            "username": creds.username,
+            "password": creds.password
+          }),
           dataType: 'JSON',
           contentType: 'application/JSON'
         };
@@ -74,10 +83,35 @@
           }
         };
       },
-      replyFlowdock: function() {
+      replyFlowdock: function(option, thread, message) {
         // get the thread or message ID
+        var data = JSON.stringify({
+          "event": "comment",
+          "content": message
+        });
+        var fd = this.store('flowdock');
+        var path = helpers.fmt('/flows/%@/%@/messages/%@/comments', option.organization.name, option.parameterized_name, thread);
+        return {
+          url: this.fdRoot + path,
+          headers: {"Authorization": "Bearer " + fd.access_token},
+          type: 'POST',
+          data: data,
+          dataType: 'JSON',
+          contentType: 'application/JSON',
+          success: function(r) {
+            this.repliedFlowdock(r, path, option);
+          }
+        };
       },
-
+      getUserFlowdock: function(id) {
+        // get the thread or message ID
+        var fd = this.store('flowdock');
+        var path = helpers.fmt('/users/%@', id);
+        return {
+          url: this.fdRoot + path,
+          headers: {"Authorization": "Bearer " + fd.access_token},
+        };
+      },
       // ### Hall
       loadHall:     function(url) {
         return {
@@ -115,13 +149,20 @@
       },
 
     },
-    // Framework event functions
-    start:          function() {
 
+    // Framework event functions
+    start: function(e) {
+      if(e) {e.preventDefault();}
       // temporary until there is a button for it
-      if(this.setting('flowdock') === true) {
-        this.loadFlowdock();
-      }
+      var flowdock;
+      // if(this.setting('flowdock') === true) {
+      //   flowdock = true
+      // } else {
+      //   flowdock
+      // }
+      this.switchTo('home', {
+        flowdock: this.setting('flowdock')
+      });
 
     },
 
@@ -132,7 +173,9 @@
         this.ajax('loadFlowdock', fd);
       } else {
         this.switchTo('login', {service: 'flowdock'});
+        this.$('button.logout').html('Login');
       }
+      this.flowdockUsers = [];
     },
     loginFlowdock:  function(e) { // on click of login button
       e.preventDefault();
@@ -153,6 +196,7 @@
       });
     },
     loadedFlowdock: function(r) {
+      this.$('button.logout').html('Logout');
       this.service = "flowdock";
       this.options = r;
       this.switchTo('form', {
@@ -182,13 +226,67 @@
     },
     postedFlowdock: function(r, flowPath, option) {
       // debugger;
-      console.log(r);
-      console.log(option);
-      var url = helpers.fmt('%@/messages/%@', option.web_url, r.id);
-      services.notify(helpers.fmt('Pinged <a href="%@" target="blank">%@ in %@</a>', url, option.name, this.service));
-      
-    },
+      console.dir(r);
+      this.url = helpers.fmt('%@/messages/%@', option.web_url, r.id);
+      services.notify(helpers.fmt('Pinged <a href="%@" target="blank">%@ in %@</a>', this.url, option.name, this.service));
 
+      this.messages = [];
+      this.listenFlowdock(r.flow, r.id, option);
+      this.switchTo('reply', {
+        service: 'flowdock',
+        url: this.url,
+        message: r
+      });
+    },
+    listenFlowdock: function(flow, messageId, option) {
+      this.flow = flow;
+      this.thread = messageId;
+      this.option = option;
+      var fd = this.store('flowdock');
+      this.stream = new EventSource( helpers.fmt('https://stream.flowdock.com/flows?filter=%@&access_token=%@', flow, fd.access_token) );
+      this.stream.onmessage = function(event) {
+        var message = JSON.parse(event.data);
+        console.dir(message);
+        var influxTags = _.find(message.tags, function(tag) {
+          return tag.match('influx:' + messageId) !== null;
+        });
+        if(influxTags) {
+          console.log("response to your message!");
+          console.log(message.content.text);
+          this.receivedResponseFlowdock(message);
+        }
+        // handle message
+
+      }.bind(this);
+    },
+    receivedResponseFlowdock: function(message) {
+      if(!this.flowdockUsers[ message.user ]) {
+        this.ajax('getUserFlowdock', message.user).done(function(r) {
+          this.flowdockUsers[r.id] = r;
+          message.userObj = r;
+          this.renderResponseFlowdock(message);
+        });
+      } else {
+        message.userObj = this.flowdockUsers[ message.user ];
+        this.renderResponseFlowdock(message);
+      }
+    },
+    renderResponseFlowdock: function(message) {
+      this.messages.push(message);
+      var html = this.renderTemplate('_responses', {
+        messages: this.messages
+      });
+      this.$('div.replies_flowdock').html(html);
+    },
+    replyFlowdock: function(e) {
+      e.preventDefault();
+      var message = this.$('textarea.reply').val();
+      this.ajax('replyFlowdock', this.option, this.thread, message);
+    },
+    repliedFlowdock: function(r) {
+      this.$('textarea.reply').val('');
+      console.dir(r);
+    },
     // Hall functions
     loadHall:       function(r) {
 
@@ -222,12 +320,23 @@
 
     },
 
+    // close
+    close: function() {
+      if(this.stream) {
+        this.stream.close();
+      }
+      
+    },
+
     // logout
     logout: function(e) {
       if(e) {e.preventDefault();}
+
       this.store('flowdock', '');
-      this.start();
-    }
+      this.$('button.logout').html('Login');
+      this.loadFlowdock();
+    },
+
     // Helper functions
 
   };
